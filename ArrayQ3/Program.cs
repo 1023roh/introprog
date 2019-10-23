a3a37e023a01974fd15068516a07a1c0b17e1efe
1023roh <102323136@student.swin.edu.au>
2019-10-23 17:52:50 +1100

    Arrayq3

---
+++ ArrayQ3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayQ3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numberList = new List<int>();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("input a number");
+                numberList.Add(int.Parse(Console.ReadLine()));
+            }
+            Console.WriteLine("Five Input numbers are");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("" + numberList[i]);
+            }
+            int counter = 0;
+            while (true)
+
+            {
+                Console.WriteLine("input another number");
+                int anotherNumber = int.Parse(Console.ReadLine());
+
+
+                if (numberList.Contains(anotherNumber))
+                {
+                    Console.WriteLine("The number exists");
+                    ++counter;
+                    Console.WriteLine("Times number repeated "+ counter);
+
+                }
+                else
+                {
+                    Console.WriteLine("this is a  new number");
+                    continue;
+                }
+
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
