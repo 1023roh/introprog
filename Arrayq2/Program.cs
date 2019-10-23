98f4a1969ec837ff5116ef0a6d2fc39d2e4c7e02
1023roh <102323136@student.swin.edu.au>
2019-10-23 17:49:25 +1100

    arrayq2

---
+++ Arrayq2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrayq2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numberArray = new int[] { 34, 5, 67, 1, 99, 34, 44, 78, 34, 0 };
+            int sum = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                sum = sum + numberArray[i];
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
